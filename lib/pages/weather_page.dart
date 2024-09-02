6d4fe49a67af25c74236067b6e960aacc98cd376
MiniCodehub <ironman102004@gmail.com>
2024-09-02 09:57:13 +0530

    textfieldAdded

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
+import 'package:hackathon_app/constant.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -24,6 +25,7 @@ class _WeatherPageState extends State<WeatherPage> {
   String apiKey = 'e114a7a0ae595a3fab28ba629489de90';
   List<String> date = [];
   String city_ = "Chennai";
+  TextEditingController userTextField = TextEditingController();
 
   Future getDate(String city) async {
     final url = Uri.parse("http://api.openweathermap.org/data/2.5/forecast");
@@ -100,6 +102,7 @@ class _WeatherPageState extends State<WeatherPage> {
         });
       },
     );
+    userTextField.text = city_;
   }
 
   @override
@@ -124,169 +127,195 @@ class _WeatherPageState extends State<WeatherPage> {
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    height: 50,
-                    child: ListView.builder(
-                      itemCount: date.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              selected = index;
-                            });
-                          },
-                          child: Card(
-                            color: selected == index ? Colors.cyan[800] : null,
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Text(
-                                "${date[index]}",
-                                style: TextStyle(fontWeight: FontWeight.w600),
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      height: 50,
+                      child: ListView.builder(
+                        itemCount: date.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selected = index;
+                              });
+                            },
+                            child: Card(
+                              color:
+                                  selected == index ? Colors.cyan[800] : null,
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Text(
+                                  "${date[index]}",
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
                   ),
-                ),
-                FutureBuilder(
-                  future: getCurrentWeather(selected, city_),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(
-                          color: Colors.white,
-                        ),
-                      );
-                    } else if (snapshot.hasError) {
-                      return const Center(child: Text("ERROR"));
-                    }
-                    return Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            width: double.infinity,
-                            child: Card(
-                              elevation: 7,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16)),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(16),
-                                child: BackdropFilter(
-                                  filter:
-                                      ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          "$currentTemp K",
-                                          style: const TextStyle(
-                                              fontSize: 32,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        const SizedBox(
-                                          height: 16,
-                                        ),
-                                        Icon(
-                                          currentSky == "Clouds"
-                                              ? Icons.cloud
-                                              : currentSky == "Rain"
-                                                  ? CupertinoIcons
-                                                      .cloud_rain_fill
-                                                  : Icons.sunny,
-                                          size: 64,
-                                        ),
-                                        const SizedBox(
-                                          height: 16,
-                                        ),
-                                        Text(
-                                          "$currentSky",
-                                          style: const TextStyle(
-                                            fontSize: 20,
+                  FutureBuilder(
+                    future: getCurrentWeather(selected, city_),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Center(child: Text("ERROR"));
+                      }
+                      return Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: Card(
+                                elevation: 7,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16)),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(16),
+                                  child: BackdropFilter(
+                                    filter:
+                                        ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "$currentTemp K",
+                                            style: const TextStyle(
+                                                fontSize: 32,
+                                                fontWeight: FontWeight.bold),
                                           ),
-                                        ),
-                                      ],
+                                          const SizedBox(
+                                            height: 16,
+                                          ),
+                                          Icon(
+                                            currentSky == "Clouds"
+                                                ? Icons.cloud
+                                                : currentSky == "Rain"
+                                                    ? CupertinoIcons
+                                                        .cloud_rain_fill
+                                                    : Icons.sunny,
+                                            size: 64,
+                                          ),
+                                          const SizedBox(
+                                            height: 16,
+                                          ),
+                                          Text(
+                                            "$currentSky",
+                                            style: const TextStyle(
+                                              fontSize: 20,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(10),
-                            alignment: Alignment.topLeft,
-                            child: const Text(
-                              "Hourly Forecast",
-                              style: TextStyle(
-                                  fontSize: 26, fontWeight: FontWeight.w500),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 125,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: forecast!.length - 1,
-                              itemBuilder: (context, index) {
-                                final foreCast = forecast![index + 1];
-                                final time = DateTime.parse(
-                                    foreCast["dt_txt"].toString());
-                                return Card_Widget(
-                                  time: DateFormat("j").format(time),
-                                  icon: foreCast["weather"][0]["main"] ==
-                                              "Clouds" ||
-                                          foreCast["weather"][0]["main"] ==
-                                              "Rain"
-                                      ? Icons.cloud
-                                      : Icons.sunny,
-                                  temp: foreCast["main"]["temp"].toString(),
-                                );
-                              },
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            padding: const EdgeInsets.only(left: 10, right: 10),
-                            alignment: Alignment.topLeft,
-                            child: const Text(
-                              "Additional Information",
-                              style: TextStyle(
-                                  fontSize: 26, fontWeight: FontWeight.w500),
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Additional_Widget(
-                                  icon: Icons.water_drop_rounded,
-                                  name: "Humidity",
-                                  number: "$humidity"),
-                              Additional_Widget(
-                                icon: Icons.air_rounded,
-                                name: "Wind Speed",
-                                number: "$windSpeed",
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              alignment: Alignment.topLeft,
+                              child: const Text(
+                                "Hourly Forecast",
+                                style: TextStyle(
+                                    fontSize: 26, fontWeight: FontWeight.w500),
                               ),
-                              Additional_Widget(
-                                  icon: Icons.beach_access_rounded,
-                                  name: "Pressure",
-                                  number: "$pressure"),
-                            ],
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ],
+                            ),
+                            SizedBox(
+                              height: 125,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: forecast!.length - 1,
+                                itemBuilder: (context, index) {
+                                  final foreCast = forecast![index + 1];
+                                  final time = DateTime.parse(
+                                      foreCast["dt_txt"].toString());
+                                  return Card_Widget(
+                                    time: DateFormat("j").format(time),
+                                    icon: foreCast["weather"][0]["main"] ==
+                                                "Clouds" ||
+                                            foreCast["weather"][0]["main"] ==
+                                                "Rain"
+                                        ? Icons.cloud
+                                        : Icons.sunny,
+                                    temp: foreCast["main"]["temp"].toString(),
+                                  );
+                                },
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              padding:
+                                  const EdgeInsets.only(left: 10, right: 10),
+                              alignment: Alignment.topLeft,
+                              child: const Text(
+                                "Additional Information",
+                                style: TextStyle(
+                                    fontSize: 26, fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Additional_Widget(
+                                    icon: Icons.water_drop_rounded,
+                                    name: "Humidity",
+                                    number: "$humidity"),
+                                Additional_Widget(
+                                  icon: Icons.air_rounded,
+                                  name: "Wind Speed",
+                                  number: "$windSpeed",
+                                ),
+                                Additional_Widget(
+                                    icon: Icons.beach_access_rounded,
+                                    name: "Pressure",
+                                    number: "$pressure"),
+                              ],
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              child: TextField(
+                                controller: userTextField,
+                                onChanged: (value) {
+                                  city_ = value;
+                                },
+                                decoration: InputDecoration(
+                                  prefixIcon: const Icon(CupertinoIcons.map),
+                                  suffixIcon: IconButton(
+                                      onPressed: () async {
+                                        await getDate(city_);
+                                        setState(() {});
+                                      },
+                                      icon: const Icon(
+                                          CupertinoIcons.cloud_download)),
+                                  hintText: "City name",
+                                  border: border,
+                                  focusedBorder: focusborder,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
     );
   }
